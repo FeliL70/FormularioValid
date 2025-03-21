daa396212f8e3339189d9dcf9f6932973e40e61a
FeliL70 <felipelipretizelaya@gmail.com>
2025-03-21 09:55:25 -0300

    Add files via upload

--- FormLipreMas/script.js
+++ FormLipreMas/script.js
@@ -3,11 +3,29 @@ const errorRequisitos = document.getElementById('errorRequisitos');
 const caracteresEspeciales = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '=', '+', '{', '}', '[', ']', '|', ':', ';', '"', '<', '>', ',', '.', '?', '/', '`', '~'];
 let confirmacion = document.getElementById('ConfirmPassword');
 const errorConfirmacion = document.getElementById('errorConfirmacion');
+const nombre = document.getElementById('Username');
 
 function contieneLetraEspecial(cadena) {
     return caracteresEspeciales.some(caracter => cadena.includes(caracter));
 }
+function NombreBien() 
+{
+    const largo = document.getElementById('largo');
+    const longitud = nombre.value.length >= 3;
 
+    if (longitud) {
+        largo.textContent = '✔ Mas de 3 caracteres';
+        largo.style.color = 'green';
+    } else {
+        largo.textContent = '✖ Mas de 3 caracteres';
+        largo.style.color = 'red';
+    }
+    if (!(longitud)) {
+        errorRequisitos.style.display = 'block';
+    } else {
+        errorRequisitos.style.display = 'none';
+    }
+}
 function ContraseñaBien() {
     const largo = document.getElementById('largo');
     const mayus = document.getElementById('mayus');
